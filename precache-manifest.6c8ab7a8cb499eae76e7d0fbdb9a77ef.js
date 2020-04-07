0fd854856b0f75c711342c325c4823d6aef64a2c
Samrath <samrathkumarmuz@gmail.com>
2020-04-07 10:03:58 +0530

    Updates

---
+++ precache-manifest.6c8ab7a8cb499eae76e7d0fbdb9a77ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4692e2f0a198ab8c7071286a61c4ff",
+    "url": "/Random-Quotes/index.html"
+  },
+  {
+    "revision": "6a6a746a4381588a7c47",
+    "url": "/Random-Quotes/static/css/main.945f91e8.chunk.css"
+  },
+  {
+    "revision": "a764ca420f4968e39d17",
+    "url": "/Random-Quotes/static/js/2.f3ac1b12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Random-Quotes/static/js/2.f3ac1b12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a6a746a4381588a7c47",
+    "url": "/Random-Quotes/static/js/main.419ff4b8.chunk.js"
+  },
+  {
+    "revision": "d1a59cf1e842014064d8",
+    "url": "/Random-Quotes/static/js/runtime-main.30b45e0f.js"
+  }
+]);
